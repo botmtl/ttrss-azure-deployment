c3aeda66a6adefb9f40283e7497fb55a7402ba18
botmtl <botmtl@gmail.com>
2017-09-01 22:03:35 -0400

    new

--- Execute-MySQLScript.ps1
+++ Execute-MySQLScript.ps1
@@ -1,65 +1,52 @@
-function Execute-MySQLScript
-{
-    [CmdletBinding()]
-    [OutputType([int])]
-    param
-    (
-        #Literal path to mysql script
-        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true,Position=0)]
-        [ValidateScript({Test-Path -LiteralPath $_ })]
-        [string]$Path,
+[CmdletBinding()]
+[OutputType([int])]
+param
+(
+    #Path to mysql script
+    [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true,Position=0,ParameterSetName="Script")]
+    [ValidateScript({Test-Path $_ })]
+    [string]$ScriptPath,
+
+    [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true,Position=0,ParameterSetName="Command")]
+    [string]$Command,
         
-        #Defaults to correct MySQLInApp connection string for this instance
-        [Parameter(Mandatory=$false)]
-        [string]$ConnectionString = {
-            [String]::Format("{0}{1}", 
-                             "SslMode=none;", 
-                             (Get-Content -LiteralPath "D:\home\data\mysql\MYSQLCONNSTR_localdb.txt").Replace(":",";Port="))
-        }
-    )
+    #Defaults to correct MySQLInApp connection string for this instance
+    [Parameter(Mandatory=$false, Position=1)]
+    [ValidateScript({ -not ($ConnectionString -eq "") })]
+    [string]$ConnectionString = 
+        [String]::Format("{0}{1}", 
+                            "SslMode=none;", 
+                            (Get-Content -LiteralPath "D:\home\data\mysql\MYSQLCONNSTR_localdb.txt").Replace(":",";Port="))
+)
 
-    Begin
-    {
-        [System.Reflection.Assembly]::LoadWithPartialName("MySql.Data")
-
-        #Open connection to localdb
-        $Connection = New-Object MySql.Data.MySqlClient.MySqlConnection -ArgumentList $ConnectionString
-        $Connection.Open()
-        Write-Debug "Connection open. (server version is $Connection.ServerVersion)"
-
-    }
-
-    Process
-    {
-        $dbInitScript = Get-Content $Path
-        $MySqlScript = New-Object -TypeName MySql.Data.MySqlClient.MySqlScript -ArgumentList $Connection, $dbInitScript
-        try {
-            $statementsCount = $MySqlScript.Execute();
-            Write-Debug "$statementsCount statements were executed."
-        }
-        catch {
-            Write-Error 'Something went wrong'.
-        }
-    }
-
-    End
-    {
-        $Connection.Close()
-    }
+#Open the MysqlConnection 
+[System.Reflection.Assembly]::LoadWithPartialName("MySql.Data")
+try {
+    $Connection = New-Object MySql.Data.MySqlClient.MySqlConnection -ArgumentList $ConnectionString
+    $Connection.Open()
+    Write-Debug "Connection open. (server version is $Connection.ServerVersion)"
+}
+catch {
+    Write-Error "Failed to open connection. ConnectionString was $ConnectionString."  
+    Throw $_.Exception
 }
 
-Execute-MySQLScript -path .\ttrss_schema.mysql
+#Prepare SQL statement(s)
+$SQL = $Command
+if ([String]::IsNullOrWhiteSpace($Command)) {
+    $SQL = Get-Content $ScriptPath
+}
 
-#
-# Script1.ps1
-#
-#[void][System.Reflection.Assembly]::LoadWithPartialName("MySql.Data")
-#$ConnectionString = Get-Content "D:\home\data\mysql\MYSQLCONNSTR_localdb.txt"
-#$ConnectionString = "SslMode=none;" + $ConnectionString
-#$ConnectionString = $ConnectionString.Replace(':',';Port=')
-#$Connection = New-Object MySql.Data.MySqlClient.MySqlConnection
-#$Connection.ConnectionString = $ConnectionString
-#$Connection.Open()
-#$dbInitScript = Get-Content "ttrss_schema.mysql"
-#$MysqlScript= New-Object MySql.Data.MySqlClient.MysqlScript -ArgumentList $Connection,$dbInitScript
-#$MysqlScript.execute()
+#Execute SQL statement(s)
+try {
+    $MySqlScript = New-Object -TypeName MySql.Data.MySqlClient.MySqlScript -ArgumentList $Connection, $SQL
+    $statementsCount = $MySqlScript.Execute();
+    Write-Debug "$statementsCount statements were executed."
+}
+catch {
+    Write-Error 'Something went wrong trying to Execute() the script.'
+    Throw $_.Exception
+}
+finally {
+    $Connection.Close()
+}
